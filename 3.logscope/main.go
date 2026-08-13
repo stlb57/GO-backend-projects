81e88095b9b55ecd819372a15e686bc8801f200d
lb57 <saarthaktyagi1@gmail.com>
2026-08-14 03:24:05 +0530

    Proj 3 mvp

--- 3.logscope/main.go
+++ 3.logscope/main.go
@@ -8,33 +8,99 @@ import (
 )
 
 type logs struct {
+	Date      string
 	Timestamp string
 	Level     string
 	Message   string
 }
 
+func (log logs) Display() {
+	fmt.Printf(
+		"Date: %s, Time: %s, Level: %s, Message: %s\n",
+		log.Date,
+		log.Timestamp,
+		log.Level,
+		log.Message,
+	)
+}
+
+type Analyzer interface {
+	Analyze([]logs) map[string]int
+}
+
+type LevelAnalyzer struct{}
+
+func (a LevelAnalyzer) Analyze(logEntries []logs) map[string]int {
+	counts := make(map[string]int)
+
+	for _, log := range logEntries {
+		counts[log.Level]++
+	}
+
+	return counts
+}
+
+type ErrorAnalyzer struct{}
+
+func (a ErrorAnalyzer) Analyze(logEntries []logs) map[string]int {
+	errors := make(map[string]int)
+
+	for _, log := range logEntries {
+		if log.Level == "ERROR" {
+			errors[log.Message]++
+		}
+	}
+
+	return errors
+}
+
+func runAnalysis(analyzer Analyzer, logEntries []logs) {
+	result := analyzer.Analyze(logEntries)
+
+	for key, count := range result {
+		fmt.Printf("%s: %d\n", key, count)
+	}
+}
+
 func main() {
 	var logEntries []logs
+
 	file := os.Args[1]
+
 	f, err := os.Open(file)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error opening file:", err)
 		return
 	}
 	defer f.Close()
+
 	scanner := bufio.NewScanner(f)
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		parts := strings.SplitN(line, " ", 3)
+
+		parts := strings.SplitN(line, " ", 4)
+
 		log := logs{
-			Timestamp: parts[0],
-			Level:     parts[1],
-			Message:   parts[2],
+			Date:      parts[0],
+			Timestamp: parts[1],
+			Level:     parts[2],
+			Message:   parts[3],
 		}
+
 		logEntries = append(logEntries, log)
 	}
+
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		fmt.Fprintln(os.Stderr, "error reading file:", err)
+		return
 	}
 
+	fmt.Println("LEVEL ANALYSIS")
+	levelAnalyzer := LevelAnalyzer{}
+	runAnalysis(levelAnalyzer, logEntries)
+
+	fmt.Println("\nERROR ANALYSIS")
+	errorAnalyzer := ErrorAnalyzer{}
+	runAnalysis(errorAnalyzer, logEntries)
 }
