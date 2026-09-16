5fea6557271613af7aba5cb28d730d2740e38405
lb57 <saarthaktyagi1@gmail.com>
2026-09-16 19:06:18 +0530

    proj 11 done

--- 11.caching/main.go
+++ 11.caching/main.go
@@ -3,17 +3,30 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type Cache struct {
-	data  map[string]CacheItem
-	mutex sync.RWMutex
+	data   map[string]CacheItem
+	mutex  sync.RWMutex
+	hits   int64
+	misses int64
 }
 
 type CacheItem struct {
 	value     string
 	expiresAt time.Time
+}
+
+func (c *Cache) Cleanup() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	for key, value := range c.data {
+		if time.Now().After(value.expiresAt) {
+			delete(c.data, key)
+		}
+	}
 }
 
 func (c *Cache) Set(key string, value string, timeout time.Time) {
@@ -32,28 +45,37 @@ func (c *Cache) Get(key string) (string, bool) {
 	value, ok := c.data[key]
 
 	if !ok {
+		atomic.AddInt64(&c.misses, 1)
 		c.mutex.RUnlock()
 		return "", false
+
 	}
 
 	if time.Now().Before(value.expiresAt) {
+		atomic.AddInt64(&c.hits, 1)
 		c.mutex.RUnlock()
 		return value.value, true
 	}
-	value, ok = c.data[key]
+
+	c.mutex.RUnlock()
+
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
+
+	value, ok = c.data[key]
+
 	if !ok {
+		atomic.AddInt64(&c.misses, 1)
 		return "", false
 	}
 
 	if time.Now().Before(value.expiresAt) {
+		atomic.AddInt64(&c.hits, 1)
 		return value.value, true
 	}
-
+	atomic.AddInt64(&c.misses, 1)
 	delete(c.data, key)
 	return "", false
-
 }
 
 func (c *Cache) Delete(key string) {
@@ -63,13 +85,33 @@ func (c *Cache) Delete(key string) {
 	delete(c.data, key)
 }
 
+type CacheStore interface {
+	Set(key string, value string, timeout time.Time)
+	Get(key string) (string, bool)
+	Delete(key string)
+	Cleanup()
+}
+
 func main() {
-	var cache_inst Cache = Cache{
+	var store CacheStore = &Cache{
 		data: map[string]CacheItem{},
 	}
 
 	var wg sync.WaitGroup
-
+	done := make(chan struct{})
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-done:
+				fmt.Println("Done!")
+				return
+			case <-ticker.C:
+				store.Cleanup()
+			}
+		}
+	}()
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 
@@ -79,13 +121,14 @@ func main() {
 			key := fmt.Sprintf("user:%d", i)
 			value := fmt.Sprintf("rahul-%d", i)
 
-			cache_inst.Set(key, value, time.Now().Add(5*time.Second))
+			store.Set(key, value, time.Now().Add(5*time.Second))
 
-			value, ok := cache_inst.Get(key)
+			value, ok := store.Get(key)
 
 			fmt.Println(key, value, ok)
 		}(i)
 	}
 
 	wg.Wait()
+	close(done)
 }
